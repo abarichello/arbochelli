70eaae27155402699fada6266352b2c13663d591
Barichello <artur@barichello.me>
2021-02-07 20:09:00 +0000

    Typo

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@
 cd ~/arbochelli && \
     git pull -r origin master && \
     DOMAIN=arbochelli.download \
-    ONION_ADDR=http://basssssxls7b72rf6b2nngxcwhndx6r2k42bcbyx6dkeps7ilh3nwlqd.onion && \
+    ONION_ADDR=http://basssssxls7b72rf6b2nngxcwhndx6r2k42bcbyx6dkeps7ilh3nwlqd.onion \
     caddy reload && \
-    date -u +"%d-%m-%y %H:%M:%S %Z" > static/update.txt
+    date -u +"%d-%m-%Y %H:%M:%S %Z" > static/update.txt
