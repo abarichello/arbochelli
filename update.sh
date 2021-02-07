07186edbb2d4d0d22d6a73417dc16a7009329b25
Barichello <artur@barichello.me>
2021-02-07 16:40:59 -0300

    Update redirect

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cd ~/arbochelli && git pull -r origin master && DOMAIN=arbochelli.download caddy reload
-
+cd ~/arbochelli && \
+    git pull -r origin master && \
+    DOMAIN=arbochelli.download \
+    ONION_ADDR=http://basssssxls7b72rf6b2nngxcwhndx6r2k42bcbyx6dkeps7ilh3nwlqd.onion && \
+    caddy reload && \
+    date -u +"%d-%m-%y %H:%M:%S %Z" > static/update.txt
