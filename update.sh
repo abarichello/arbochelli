7cca455ef787354acd57bbcfa5d9693807f79749
Barichello <artur@barichello.me>
2021-02-18 00:56:37 -0300

    Update domain

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 cd ~/arbochelli && \
     git pull -r origin master && \
-    DOMAIN=arbochelli.download \
+    DOMAIN=arbochelli.me \
     ONION_ADDR=http://basssssxls7b72rf6b2nngxcwhndx6r2k42bcbyx6dkeps7ilh3nwlqd.onion \
     caddy reload && \
     date -u +"%d-%m-%Y %H:%M:%S %Z" > static/update.txt
