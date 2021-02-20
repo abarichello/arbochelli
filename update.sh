176c22e453149a59ce60f0d2a733a26566fa9ac3
Barichello <artur@barichello.me>
2021-02-20 14:01:54 -0300

    Add commit info to update.txt

--- update.sh
+++ update.sh
@@ -5,4 +5,5 @@ cd ~/arbochelli && \
     DOMAIN=arbochelli.me \
     ONION_ADDR=http://basssssxls7b72rf6b2nngxcwhndx6r2k42bcbyx6dkeps7ilh3nwlqd.onion \
     caddy reload && \
-    date -u +"%d-%m-%Y %H:%M:%S %Z" > static/update.txt
+    date -u +"%d-%m-%Y %H:%M:%S %Z" > static/update.txt && \
+    git log HEAD --format=reference --no-decorate -n1 >> static/update.txt
