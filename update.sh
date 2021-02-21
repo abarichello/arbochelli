72d658beaa82cd4155d0033fb572059441dd5b2e
Barichello <artur@barichello.me>
2021-02-21 01:07:39 -0300

    Update CSS

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ cd ~/arbochelli && \
     ONION_ADDR=http://basssssxls7b72rf6b2nngxcwhndx6r2k42bcbyx6dkeps7ilh3nwlqd.onion \
     caddy reload && \
     date -u +"%d-%m-%Y %H:%M:%S %Z" > static/update.txt && \
-    git log HEAD --format=reference --no-decorate -n1 >> static/update.txt
+    git log --format=reference --no-decorate -n1 >> static/update.txt
