941ad54112796670eaaa3ab6fb645e7be4fd9a6b
Barichello <artur@barichello.me>
2021-03-04 00:53:17 -0300

    Haxe deploy

--- update.sh
+++ update.sh
@@ -2,8 +2,11 @@
 
 cd ~/arbochelli && \
     git pull -r origin master && \
+    haxe src/build.hxml && \
+
     DOMAIN=arbochelli.me \
     ONION_ADDR=http://basssssxls7b72rf6b2nngxcwhndx6r2k42bcbyx6dkeps7ilh3nwlqd.onion \
     caddy reload && \
+
     date -u +"%d-%m-%Y %H:%M:%S %Z" > static/update.txt && \
     git log --format=reference --no-decorate -n1 >> static/update.txt
