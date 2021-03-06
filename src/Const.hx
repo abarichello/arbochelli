1c0783df58dbb4a730a419330d10fdc939fb07f1
Barichello <artur@barichello.me>
2021-03-06 19:09:11 -0300

    Add youtube parser

--- src/Const.hx
+++ src/Const.hx
@@ -7,3 +7,4 @@ inline var DOMAIN = "arbochelli.me";
 #end
 inline var DOMAIN_URL = 'https://${DOMAIN}/';
 inline var COLLECT_URL = 'https://log.${DOMAIN}/api/collect';
+inline var FEED_URL = DOMAIN_URL + "static/yt-rss.xml";
