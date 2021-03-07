6d6804bd13d9b6b171fd869fb609c4b3ef2f9491
Barichello <artur@barichello.me>
2021-03-06 22:57:22 -0300

    Add recent videos section

--- src/Main.hx
+++ src/Main.hx
@@ -3,12 +3,12 @@ package src;
 import js.Browser;
 
 function main() {
+    src.Youtube.getRSSFeed();
+    src.Template.replaceDash();
     src.Template.bindEvents();
     src.Template.initFilter();
     src.Template.getFilterElement().focus();
-    src.Template.replaceDash();
     for (t in Browser.document.getElementsByTagName("time")) {
         src.Template.localizeDateTime(t);
     }
-    src.Youtube.getRSSFeed();
 }
--- src/Youtube.hx
+++ src/Youtube.hx
@@ -1,33 +1,66 @@
 package src;
 
-import haxe.xml.Access;
+import js.html.SpanElement;
 import haxe.xml.Parser;
+import haxe.xml.Access;
 import haxe.http.HttpJs;
+import js.lib.Date;
+import js.Browser.location;
+import js.Browser.document;
+
+typedef ParseResults = {
+    totalVideos: Int,
+    videos: Array<{
+        title: String,
+        link: String,
+        thumbnail: String,
+        published: String,
+    }>,
+}
 
 function getRSSFeed() {
     var req = new HttpJs(src.Const.FEED_URL);
     req.async = true;
-    req.onData = parseFeed;
+    req.onData = onData;
     req.onError = onError;
     req.request(false);
 }
 
-function parseFeed(feed: String) {
+function onError(err: String) {
+    trace(err);
+}
+
+function onData(feed: String) {
+    updateLatestVideos(parseFeed(feed));
+}
+
+function parseFeed(feed: String): ParseResults {
     feed = StringTools.replace(feed, "\n  ", "");
     var xml = Parser.parse(feed);
     var entries = new Access(xml.firstElement()).nodes.entry;
-
-    var results = entries.map((entry) -> {
-        return {
-            title: StringTools.replace(entry.node.title.innerData, " [Bass Tab+Sheet]", ""),
-            link: entry.node.link.att.href,
-            thumbnail: entry.node.resolve("media:group").node.resolve("media:description").innerData,
+    var videos = [for (i in 0...5)
+        {
+            title: StringTools.replace(entries[i].node.title.innerData, " [Bass Tabs/Sheet]", ""),
+            link: entries[i].node.link.att.href,
+            thumbnail: entries[i].node.resolve("media:group").node.resolve("media:description").innerData,
+            published: new Date(entries[i].node.published.innerData).toLocaleDateString(),
         }
-    });
-    trace(results);
-    return results;
+    ];
+    return {
+        totalVideos: entries.length,
+        videos: videos,
+    };
 }
 
-function onError(err: String) {
-    trace(err);
+function updateLatestVideos(results: ParseResults) {
+    if (location.pathname == "/") {
+        updateTotalText(results.totalVideos);
+    }
+
+    var videosHTML = results.videos.map((video) -> '<a href="${video.link}" title="Published: ${video.published}"><u>${video.title}</u></a>');
+    document.getElementById("recent-videos").innerHTML = '<b>Recent videos: ${videosHTML.join(" | ")}</b>';
+}
+
+function updateTotalText(totalScores: Int) {
+    document.getElementById("total-text").innerHTML = '<b>${Std.string(totalScores)}</b> videos';
 }
