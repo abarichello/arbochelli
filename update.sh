5adb80385676c11dcd364a82a512b08989ef47bd
Barichello <artur@barichello.me>
2021-03-10 00:55:34 -0300

    Update status page

--- update.sh
+++ update.sh
@@ -7,7 +7,12 @@ DOMAIN=arbochelli.me \
 DOMAIN2=arbochelli.download \
 ONION_ADDR=http://basssssxls7b72rf6b2nngxcwhndx6r2k42bcbyx6dkeps7ilh3nwlqd.onion \
 caddy reload && \
-
 curl 'https://www.youtube.com/feeds/videos.xml?channel_id=UCQyPHw4V7du8Fx-o12_fudw' > static/yt-rss.xml && \
-date -u +"%d-%m-%Y %H:%M:%S %Z" > static/update.txt && \
-git log --format=reference --no-decorate -n1 >> static/update.txt
+
+printf "Info|Value\n-|-\nLast updated|%s\nLast commit|%s\nRepo size|%s\nDisk size|%s" \
+    "$(date -u +"%d-%m-%Y %H:%M:%S %Z")" \
+    "$(git log --format=reference --no-decorate -n1)" \
+    "$(git count-objects -vH | awk '{if (NR==5) print $2$3}')" \
+    "$(du -sh . | awk '{print $1}')" \
+    > static/status.md && \
+    pandoc -s static/status.md -c static/template.css --metadata title="status" -o static/status.html
