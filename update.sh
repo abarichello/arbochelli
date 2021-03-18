06b4c14ce6d9edff1b8955eae69c2d360f736a7f
Barichello <artur@barichello.me>
2021-03-18 01:10:39 -0300

    Update ghostwriter deploy

--- update.sh
+++ update.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+# TODO: transform this into a Dockerfile
+
 git pull -r origin master && \
 git submodule update --recursive && \
-haxe src/build.hxml && \
-rm -rf blog/public/css && \
+curl 'https://www.youtube.com/feeds/videos.xml?channel_id=UCQyPHw4V7du8Fx-o12_fudw' > static/yt-rss.xml && \
+haxe src/build/all.hxml && \
 hexo generate --cwd blog && \
 
 DOMAIN=arbochelli.me \
 DOMAIN2=arbochelli.download \
 ONION_ADDR=http://basssssxls7b72rf6b2nngxcwhndx6r2k42bcbyx6dkeps7ilh3nwlqd.onion \
 caddy reload && \
-curl 'https://www.youtube.com/feeds/videos.xml?channel_id=UCQyPHw4V7du8Fx-o12_fudw' > static/yt-rss.xml && \
 
 printf "Info|Value\n-|-\n**Last updated**|%s\n**Last commit**|%s\n**Repo size**|%s\n**Disk size**|%s" \
     "$(date -u +"%d-%m-%Y %H:%M:%S %Z")" \
