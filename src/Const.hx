65e383056490e508ad7c70e79358f92512e8a43f
Barichello <artur@barichello.me>
2021-03-28 01:40:49 -0300

    Update macros

--- src/Const.hx
+++ src/Const.hx
@@ -1,10 +1,6 @@
 package src;
 
-#if debug
-inline final DOMAIN = "localhost";
-#else
-inline final DOMAIN = "arbochelli.me";
-#end
+inline final DOMAIN = #if debug "localhost"; #else "arbochelli.me"; #end
 inline final DOMAIN_URL = 'https://${DOMAIN}/';
 inline final COLLECT_URL = 'https://log.${DOMAIN}/api/collect';
 inline final FEED_URL = DOMAIN_URL + "static/yt-rss.xml";
