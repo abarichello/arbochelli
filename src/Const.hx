0557e8a0326a388af2edece16b8da35ede4fa599
Barichello <artur@barichello.me>
2021-10-28 22:39:58 -0300

    Update domain const

--- src/Const.hx
+++ src/Const.hx
@@ -1,6 +1,6 @@
 package src;
 
-inline final DOMAIN = #if debug "localhost"; #else "arbochelli.me"; #end
+inline final DOMAIN = #if debug "localhost"; #else "aa.art.br"; #end
 inline final DOMAIN_URL = 'https://${DOMAIN}/';
 inline final COLLECT_URL = 'https://log.${DOMAIN}/api/collect';
 inline final FEED_URL = DOMAIN_URL + "static/yt-rss.xml";
