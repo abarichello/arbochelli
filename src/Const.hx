69db050a360be4742f6bafc9465580cf005cb057
Barichello <artur@barichello.me>
2022-06-19 04:37:21 -0300

    Update Const.hx URLs

--- src/Const.hx
+++ src/Const.hx
@@ -1,6 +1,8 @@
 package src;
 
-inline final DOMAIN = #if debug "localhost"; #else "aa.art.br"; #end
+inline final DOMAIN = #if debug "localhost"; #else "arbochelli.me"; #end
+inline final SHORT_DOMAIN = #if debug "a.localhost"; #else "aa.art.br"; #end
 inline final DOMAIN_URL = 'https://${DOMAIN}/';
-inline final COLLECT_URL = 'https://log.${DOMAIN}/api/collect';
+inline final SHORT_DOMAIN_URL = 'https://${SHORT_DOMAIN}/';
+inline final COLLECT_URL = 'https://log.${SHORT_DOMAIN}/api/collect';
 inline final FEED_URL = DOMAIN_URL + "static/yt-rss.xml";
