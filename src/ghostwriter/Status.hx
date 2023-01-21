1916ec88a6918affc4678d1ef0313486cafe9f5f
Barichello <artur@barichello.me>
2023-01-21 15:54:19 -0300

    Refactor Status.hx

--- src/ghostwriter/Status.hx
+++ src/ghostwriter/Status.hx
@@ -3,30 +3,23 @@ package src.ghostwriter;
 import sys.io.File;
 
 function generateStatusFile() {
-    var commands = [
-        "date -u +'%d-%m-%Y %H:%M:%S %Z'",
-        "git log --format=short --no-decorate -n1 --oneline",
-        "git count-objects -vH | awk '{if (NR==5) print $2$3}'",
-        "du -sh . | awk '{print $1}'",
-        "ls -1 pdf | wc -l",
-        "ls -1 blog/source/_posts/ | wc -l",
+    var map: Map<String, String> = [
+        "Last updated" => "date -u +'%d-%m-%Y %H:%M:%S %Z'",
+        "Last commit" => "git log --format=short --no-decorate -n1 --oneline",
+        "Repo size" => "git count-objects -vH | awk '{if (NR==5) print $2$3}'",
+        "Disk size" => "du -sh . | awk '{print $1}'",
+        "Total PDFs" => "ls -1 pdf | wc -l",
+        "Total blog posts" => "ls -1 blog/source/_posts/ | wc -l",
     ];
-    final output = [];
-    for (cmd in commands) {
-        final process = new sys.io.Process(cmd);
-        Utils.assert(process.exitCode() == 0, "Exit code different than zero");
-        output.push(process.stdout.readLine());
+    var markdown = "Info|Value\n-|-\n";
+    for (txt in map.keys()) {
+        final cmd = map.get(txt);
+        if (cmd != null) {
+            final process = new sys.io.Process(cmd);
+            Utils.assert(process.exitCode() == 0, "Exit code different than zero");
+            markdown += '**$txt**|${process.stdout.readLine()}\n';
+        }
     }
-    final markdown = '
-Info|Value
--|-
-**Last updated**|${output[0]}
-**Last commit**|${output[1]}
-**Repo size**|${output[2]}
-**Disk size**|${output[3]}
-**Total PDFs**|${output[4]}
-**Total blog posts**|${output[5]}
-';
     File.saveContent('static/status.md', markdown);
     trace("Saved status.md");
 }
